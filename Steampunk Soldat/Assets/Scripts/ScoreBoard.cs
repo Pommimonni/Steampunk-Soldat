0c85cca56f3d055119a2a87adbf047fef0463876
Lauri Lehtonen <lauri.lehtonen@aalto.fi>
2015-10-11 16:33:20 +0300

    Scoreboard graphics, splashscreen

--- Steampunk Soldat/Assets/Scripts/ScoreBoard.cs
+++ Steampunk Soldat/Assets/Scripts/ScoreBoard.cs
@@ -4,32 +4,59 @@ using System.Collections.Generic;
 
 public class ScoreBoard : MonoBehaviour {
 
-    public Text score;
     List<PlayerScore> allScores;
+    public Image bg;
+    public GameObject rowBg;
+    public GameObject scoreRowPrefab;
+    public GameObject scoreBoardUI;
+
+    public List<GameObject> scoreRows;
+    bool scoresVisible = false;
     // Use this for initialization
     void Start () {
-	    
-	}
+        scoreRows = new List<GameObject>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         //bg.SetActive(Input.GetKey(KeyCode.Tab));
-        score.enabled = Input.GetKey(KeyCode.Tab);
-        if (Input.GetKey(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
-            GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
-            allScores = new List<PlayerScore>();
-            foreach(GameObject player in allPlayers)
+            scoresVisible = true;
+            scoreBoardUI.SetActive(true);
+
+            allScores = FindAllScores();
+            
+            foreach(GameObject go in scoreRows)
             {
-                allScores.Add(player.GetComponent<PlayerScore>());
+                Destroy(go);
             }
-            score.text = "";
-            foreach(PlayerScore nScore in allScores)
+            scoreRows = new List<GameObject>();
+            float yPos = 1;
+            foreach (PlayerScore nScore in allScores)
             {
-                score.text += "Player "+nScore.playerID+": Kills: "+nScore.kills+" Deaths: "+nScore.deaths+"\n";
+                GameObject newScoreRow = (GameObject)Instantiate(scoreRowPrefab, new Vector3(0, -(yPos++)*35 - 120, 0), Quaternion.identity);
+                scoreRows.Add(newScoreRow);
+                newScoreRow.GetComponentInChildren<Text>().text += "Player "+nScore.playerID+": Kills: "+nScore.kills+" Deaths: "+nScore.deaths+"\n";
+                newScoreRow.transform.SetParent(scoreBoardUI.transform, false);
             }
-            score.text += "End of score board text";
+        } else if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            scoreBoardUI.SetActive(false);
+            scoresVisible = false;
         }
+        
+    }
+
+    List<PlayerScore> FindAllScores()
+    {
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        List<PlayerScore> allScoresFound = new List<PlayerScore>();
+        foreach (GameObject player in allPlayers)
+        {
+            allScoresFound.Add(player.GetComponent<PlayerScore>());
+        }
+        return allScoresFound;
     }
 }
