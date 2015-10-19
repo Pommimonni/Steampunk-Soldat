e573dc7e70b4448a52e8df1fac829a9fea10968e
Pommimonni <lauri.lehtonen@aalto.fi>
2015-10-19 17:00:34 +0800

    Gun sounds, minigun winding

--- Steampunk Soldat/Assets/Scripts/MinigunSpec.cs
+++ Steampunk Soldat/Assets/Scripts/MinigunSpec.cs
@@ -6,8 +6,71 @@ public class MinigunSpec : WeaponBase
 {
     
     public float kickback = 1;
+    public float chargeTime = 0.5f;
+    public MultiPhaseAudio multiAudio;
+
+    int shootingPhase = 1;
+
+    bool buttonDown = false;
+    float currentChargeTime = 0;
     
-    
+
+    void Update()
+    {
+        if (!weaponOwner.GetComponent<NetworkIdentity>().isLocalPlayer)
+            return;
+        if (Input.GetMouseButton(0) && !reloading)
+        {
+            if(shootingPhase == 1 || shootingPhase == 0)
+            {
+                chargeShooting();
+            } else
+            {
+                ShootingRequest(); //sends a request to server to shoot
+            }
+        } else
+        {
+            if(shootingPhase == 2)
+            {
+                multiAudio.Play(3);
+            }
+            if(currentChargeTime > 0)
+            {
+                currentChargeTime -= Time.deltaTime;
+            } else
+            {
+                currentChargeTime = 0;
+                multiAudio.ResetToStart();
+            }
+            shootingPhase = 0;
+            
+        }
+    }
+
+    void chargeShooting()
+    {
+        if(shootingPhase == 0)
+        {
+            multiAudio.Play(1);
+            shootingPhase = 1;
+        } else
+        {
+            currentChargeTime += Time.deltaTime;
+            if(currentChargeTime > chargeTime)
+            {
+                shootingPhase = 2;
+                multiAudio.Play(2);
+                currentChargeTime = chargeTime;
+            }
+        }
+    }
+
+
+    public override void ShootSound()
+    {
+        //removes the basic shoot sound
+    }
+
     public override void HandleShoot(Vector3 from, Vector3 towards)
     {
         if (weaponOwner.GetComponent<NetworkIdentity>().isLocalPlayer)
---
+++ Steampunk Soldat/Assets/Scripts/MultiPhaseAudio.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiPhaseAudio : MonoBehaviour {
+
+    public AudioClip[] audioClips;
+    public AudioSource audioSource;
+
+    int lastPhase = 0;
+    int phaseAmount = 0;
+
+    // Use this for initialization
+    void Start () {
+        phaseAmount = audioClips.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Play(int phase)
+    {
+        if(phase > phaseAmount || phase < 1)
+        {
+            Debug.LogError("Phase " + phase + " doesnt exist! Only "+phaseAmount+" phases added in the script");
+            return;
+        }
+        lastPhase = phase;
+        int phaseIndex = phase - 1;
+        audioSource.clip = audioClips[phaseIndex];
+        audioSource.Play();
+    }
+    
+    public void Play()
+    {
+        if (lastPhase < 0)
+        {
+            Debug.LogError("Calling a negative phase! dont do this!");
+            return;
+        }
+        int nextPhase = lastPhase + 1;
+        if(nextPhase > phaseAmount)
+        {
+            nextPhase = 1;
+        }
+        Play(nextPhase);
+    }
+
+    public void ResetToStart()
+    {
+        audioSource.Stop();
+        lastPhase = 0;
+    }
+
+    void StopCurrentPhase()
+    {
+        audioSource.Stop();
+    }
+
+    void StartNextPhase()
+    {
+
+    }
+}
